953879f7d31151dfe212dbcf642e897ae9af2f73
gabrielgarciamarquez <raziel8008@gmail.com>
2021-07-04 21:00:18 -0500

    primer commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+*Main template file
+*
+*@package getwebsya
+*/
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Getwebsya Theme</title>
+</head>
+<body>
+	
+</body>
+</html>
